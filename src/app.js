5fbb56096b2fe6e076d1c8dc39209e43147ecc9c
Sonny Naidja <sonny.naidja@protonmail.com>
2025-10-25 10:25:03 +0200

    [+] develop: add test connection to mongodb

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const User = require('./features/users/users.model');
+require('dotenv').config();
 const path = require("path");
 const app = express();
 
 app.use(express.json());
+
+app.post('/users', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
