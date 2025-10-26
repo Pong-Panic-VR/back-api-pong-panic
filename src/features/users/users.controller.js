11d314f8a41e84289eef3c25ecca424b9ee48cfb
Sonny Naidja <sonny.naidja@protonmail.com>
2025-10-26 18:29:34 +0100

    [+] feature/users: hash added on create new user

--- src/features/users/users.controller.js
+++ src/features/users/users.controller.js
@@ -1,8 +1,17 @@
 const User = require('./users.model');
+const bcryptjs = require('bcryptjs');
+require('dotenv').config()
+
+const pepper = process.env.PEPPER;
 
 exports.createUser = async (req, res) => {
     try {
-        const newUser = new User(req.body);
+        const hash = bcryptjs.hashSync(req.body.password + pepper, 10);
+        const newUser = new User ({
+            email: req.body.email,
+            password: hash,
+            username: req.body.username
+        });
         await newUser.save();
         res.status(201).json(newUser);
     } catch (err) {
